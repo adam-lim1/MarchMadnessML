76d62ae6b32d7520a9bec13a76ad8477444f8c5d
adam-lim1 <adam.lim.1@gmail.com>
2021-03-28 12:55:43 -0500

    Create function to score current season bracket

--- mmml/mmml/config.py
+++ mmml/mmml/config.py
@@ -1,5 +1,6 @@
 import os
 
 data_folder = 'google-cloud-ncaa-march-madness-2020-division-1-mens-tournament'
-oot_years = [2017, 2018, 2019]
+oot_years = [2017, 2018, 2019, 2021]
+current_season = 2021
 log_location = os.path.join(os.path.dirname(os.getcwd()), 'Output/out.log')
